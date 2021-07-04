368a95905bda9c2376000a899ced1538efad5f09
Chuche <chuchin1770@gmail.com>
2021-07-03 22:20:41 -0500

    Hola mundo

--- db/procedures.sql
+++ db/procedures.sql
@@ -9,20 +9,24 @@ CREATE PROCEDURE `USUARIOS` (
   IN _email VARCHAR(50),
   IN _phone CHAR(10),
   IN _pin CHAR(4),
-  IN _answerSQ VARCHAR(45)
+  IN _answerSQ VARCHAR(45),
   IN _idSQ INT
 )
 BEGIN 
   IF _id = 0 THEN
-    INSERT INTO employee (nombre, correo,telefonoPersonal,pin,respuestaPreguntaSecreta,PREGUNTAS_SECRETAS_ID)
+    INSERT INTO USUARIOS (nombre, correo,telefonoPersonal,pin,respuestaPreguntaSecreta,PREGUNTAS_SECRETAS_ID)
     VALUES (_name, _email,_phone,_pin,_answerSQ,_idSQ);
 
     SET _id = LAST_INSERT_ID();
   ELSE
-    UPDATE employee
+    UPDATE USUARIOS
     SET
-    name = _name,
-    salary = _salary
+    nombre = _name,
+    correo = _email,
+    telefonoPersonal=_phone,
+    pin=_pin,
+    respuestaPreguntaSecreta=_answerSQ,
+    PREGUNTAS_SECRETAS_ID=_idSQ
     WHERE id = _id;
   END IF;
 
